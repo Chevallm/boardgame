b3795deba8a8bdf07ed0d5af55d280bb0af0ecd3
mchevallierpichon <maxime.chevallier-pichon@atinternet.com>
2023-01-26 14:19:35 +0100

    floor randomness

--- tiled/Floor32.tsx
+++ tiled/Floor32.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Floor" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="../assets/Mini_Dungeon_v1.0/Tilesets/Floor.png" width="64" height="64"/>
+ <tile id="0" probability="0.1"/>
+ <tile id="1" probability="0.7"/>
+ <tile id="2" probability="0.1"/>
+ <tile id="3" probability="0.1"/>
 </tileset>
